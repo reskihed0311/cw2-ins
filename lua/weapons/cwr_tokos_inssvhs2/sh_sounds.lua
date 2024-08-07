9d6439f3daa6d24a5bc7fd400884b4653188c489
NotTrashraD <bellie1579@gmail.com>
2024-08-08 01:55:39 +0200

    add vhs2

---
+++ lua/weapons/cwr_tokos_inssvhs2/sh_sounds.lua
@@ -0,0 +1,4 @@
+
+
+CustomizableWeaponry:addFireSound("TOKOS_INSSVHS_FIRE", "weapons/tokos/weapons/inss_vhs2/fire_fp.wav", 1, 115, CHAN_WEAPON)
+CustomizableWeaponry:addFireSound("TOKOS_INSSVHS_FIRE_SUPPRESSED", "weapons/tokos/weapons/inss_vhs2/fire_sup.wav", 1, 90, CHAN_WEAPON)
